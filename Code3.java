c4db1a911314f1b6649efe8293c5b567c87320d9
Swapnil Ramdas Kanherkar <123960430+Swapnilkanherkar2002@users.noreply.github.com>
2026-03-14 12:52:32 +0530

    Add files via upload

---
+++ Code3.java
@@ -0,0 +1,19 @@
+public class Code3 {
+    public static void main(String[] args) {
+        int n = 6; 
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print(1);
+                } else {
+                    System.out.print(0);
+                }
+                if (j < i) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
